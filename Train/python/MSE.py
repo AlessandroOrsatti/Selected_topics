b65a99acb5a5ab4b72a20105047d9dad853bc4b1
AlessandroOrsatti <94984780+AlessandroOrsatti@users.noreply.github.com>
2023-08-23 21:18:14 +0200

    Create MSE.py

---
+++ Train/python/MSE.py
@@ -0,0 +1,49 @@
+
+import numpy as np
+import soundfile as sf
+
+desired_samplerate = 44100
+
+#Original file 
+Original_path =
+Original, samplerate = sf.read(Original_path, dtype='float32')
+
+if samplerate != desired_samplerate:       
+    print("load_wav_file warning: sample rate wrong, resampling from ", samplerate, "to", desired_samplerate)
+    Original = sf.resample(Original, target_samplerate=desired_samplerate)
+
+if len(Original.shape) > 1 and Original.shape[1] > 1:
+    Original = Original[:, 0]
+
+Original = np.array(Original)
+
+#Test files
+test_folder = 
+assert os.path.exists(test_folder), "Audio folder  not found. Looked for " + test_folder
+
+test_list = []
+for file in os.listdir(test_folder):
+    if file.endswith(".wav"):
+        test_list.append(os.path.join(test_folder, file))
+
+test = []
+for file_path in test_list:
+    data, samplerate = sf.read(file_path, dtype='float32')
+
+    if samplerate != desired_samplerate:       
+        print("load_wav_file warning: sample rate wrong, resampling from ", samplerate, "to", desired_samplerate)
+        data = sf.resample(data, target_samplerate=desired_samplerate)
+
+    if len(data.shape) > 1 and data.shape[1] > 1:
+        data = data[:, 0]
+    test.append(data)
+    
+
+#MSE
+counter = 1
+for sample in test:
+    sample = np.array(sample)
+    mse = (np.square(Original - sample)).mean(axis=ax)
+    print("The mse for the test file number", counter, "is equal to: ", mse)
+    
+    
